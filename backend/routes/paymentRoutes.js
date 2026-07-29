b3b0c807bfb5d6e7fd6178058a42cc7b468acfa0
XDevs-Programming <callumcraft2009@gmail.com>
2026-07-29 18:53:29 +0100

    Update paymentRoutes.js

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -8,6 +8,11 @@ router.get("/mine", protect, controller.getMyPayments);
 router.get("/", protect, requireRole("admin"), controller.getAllPayments);
 router.post("/", protect, requireRole("admin"), controller.createPaymentRequest);
 router.post("/:id/checkout", protect, controller.createCheckoutSession);
-router.patch("/:id/cancel", protect, requireRole("admin"), controller.cancelPaymentRequest);
+router.patch(
+  "/:id/cancel",
+  protect,
+  requireRole("admin"),
+  controller.cancelPaymentRequest
+);
 
 module.exports = router;
