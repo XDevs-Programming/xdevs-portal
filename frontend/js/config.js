5b35cfdce9afc3e716ac85fcadb75cf2e7ff02b2
X-Devs Programming <c.bartaby09@gmail.com>
2026-09-08 22:53:40 +0100

    v6.5.1 Landing & Nav Redesign

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -4,7 +4,7 @@ window.APP_CONFIG = Object.freeze({
   CALLBACK_PAGE: "/pages/auth/callback.html",
   CLIENT_DASHBOARD: "/pages/dashboard/client.html",
   ADMIN_DASHBOARD: "/pages/dashboard/admin.html",
-  VERSION: "6.5.0",
+  VERSION: "6.5.1",
   MAINTENANCE_MODE: false,
   RELEASE_DATE: "8 September 2026",
   LEGAL_CONTACT_EMAIL: "xdevs.programming@gmail.com",
