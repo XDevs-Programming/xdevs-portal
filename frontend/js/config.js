2f1a6ffda028aeeff8228d8fae44380b4681523d
X-Devs Programming <c.bartaby09@gmail.com>
2026-09-08 22:03:06 +0100

    v6.4.1 email hotfix

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -4,7 +4,7 @@ window.APP_CONFIG = Object.freeze({
   CALLBACK_PAGE: "/pages/auth/callback.html",
   CLIENT_DASHBOARD: "/pages/dashboard/client.html",
   ADMIN_DASHBOARD: "/pages/dashboard/admin.html",
-  VERSION: "6.4.0",
+  VERSION: "6.4.1",
   MAINTENANCE_MODE: false,
   RELEASE_DATE: "8 September 2026",
   LEGAL_CONTACT_EMAIL: "xdevs.programming@gmail.com",
